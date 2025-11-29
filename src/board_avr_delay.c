9d54c255cff03316e03f470db10585579d1f6d11
Camren Chraplak <camren@duck.com>
2025-11-29 15:27:48 -0500

    Added Arduino delay

--- src/board_avr_delay.c
+++ src/board_avr_delay.c
@@ -17,88 +17,104 @@
 */
 
 #include <osc_common/common_thread.h>
-
-//#define __DELAY_BACKWARD_COMPATIBLE__
-#include <util/delay.h>
 #include <osc_common/common_timer.h>
 
-//#define MS_TO_US 1000
-#define TIME_UNIT_CONVERT 1000
+#if defined(ARDUINO) && !defined(OVERRIDE_ARDUINO_TIMER)
+	#include <Arduino.h>
+#endif
 
-volatile uint32_t delayCount = 0U;
+#if !defined(ARDUINO) || defined(OVERRIDE_ARDUINO_TIMER)
 
-void RUN_IN_RAM(delayCounter) delayCounter(void *emptyParams) {
-	delayCount++;
-}
+	//#define __DELAY_BACKWARD_COMPATIBLE__
+	#include <util/delay.h>
+
+	#define TIME_UNIT_CONVERT 1000
+
+	volatile uint32_t delayCount = 0U;
+
+	void RUN_IN_RAM(delayCounter) delayCounter(void *emptyParams) {
+		delayCount++;
+	}
+
+#endif
 
 void hardDelayMS(uint32_t delayAmount) {
-	hard_timer_t timer = HARD_TIMER_INVALID;
-	freq_t freq = 0;
 
-	uint16_t leftover = 0;
+	#if defined(ARDUINO) && !defined(OVERRIDE_ARDUINO_TIMER)
+		delay(delayAmount);
+	#else
+		hard_timer_t timer = HARD_TIMER_INVALID;
+		freq_t freq = 0;
 
-	// delay s
-	if (delayAmount >= TIME_UNIT_CONVERT) {
-		leftover = delayAmount % TIME_UNIT_CONVERT;
-		delayCount = 0;
+		uint16_t leftover = 0;
 
-		freq = 1;
-		
-		// try 1Hz
-		if (!setHardTimer(&timer, &freq, &delayCounter, NULL, DEFAULT_HARD_TIMER_PRIORITY)) {
-			freq = 100;
+		// delay s
+		if (delayAmount >= TIME_UNIT_CONVERT) {
+			leftover = delayAmount % TIME_UNIT_CONVERT;
+			delayCount = 0;
+
+			freq = 1;
+			
+			// try 1Hz
+			if (!setHardTimer(&timer, &freq, &delayCounter, NULL, DEFAULT_HARD_TIMER_PRIORITY)) {
+				freq = 100;
+				cancelHardTimer(timer);
+				timer = HARD_TIMER_INVALID;
+
+				// try 100Hz
+				if (!setHardTimer(&timer, &freq, &delayCounter, NULL, DEFAULT_HARD_TIMER_PRIORITY)) {
+					cancelHardTimer(timer);
+					return;
+				}
+			}
+
+			while (delayCount < (delayAmount - leftover) * freq / TIME_UNIT_CONVERT) {}
 			cancelHardTimer(timer);
+		}
+
+		// delay ms
+		if (leftover > 0) {
+			freq = TIME_UNIT_CONVERT;
+			delayCount = 0;
 			timer = HARD_TIMER_INVALID;
 
-			// try 100Hz
 			if (!setHardTimer(&timer, &freq, &delayCounter, NULL, DEFAULT_HARD_TIMER_PRIORITY)) {
 				cancelHardTimer(timer);
 				return;
 			}
-		}
-
-		while (delayCount < (delayAmount - leftover) * freq / TIME_UNIT_CONVERT) {}
-		cancelHardTimer(timer);
-	}
-
-	// delay ms
-	if (leftover > 0) {
-		freq = TIME_UNIT_CONVERT;
-		delayCount = 0;
-		timer = HARD_TIMER_INVALID;
-
-		if (!setHardTimer(&timer, &freq, &delayCounter, NULL, DEFAULT_HARD_TIMER_PRIORITY)) {
+			while (delayCount < leftover * freq / TIME_UNIT_CONVERT) {}
 			cancelHardTimer(timer);
-			return;
 		}
-		while (delayCount < leftover * freq / TIME_UNIT_CONVERT) {}
-		cancelHardTimer(timer);
-	}
+	#endif
 }
 
 void hardDelayUS(uint32_t delayAmount) {
 
-	// TODO: implement us delay
+	#if defined(ARDUINO) && !defined(OVERRIDE_ARDUINO_TIMER)
+		delayMicroseconds(delayAmount);
+	#else
+		// TODO: implement us delay
 
-	//_delay_us(delayAmount);
+		//_delay_us(delayAmount);
 
-	/*if (delayAmount >= TIME_UNIT_CONVERT) {
-		hardDelayMS(delayAmount / TIME_UNIT_CONVERT);
-		delayAmount = delayAmount % TIME_UNIT_CONVERT;
-	}
+		/*if (delayAmount >= TIME_UNIT_CONVERT) {
+			hardDelayMS(delayAmount / TIME_UNIT_CONVERT);
+			delayAmount = delayAmount % TIME_UNIT_CONVERT;
+		}
 
-	while (delayAmount >= 100) {
-		_delay_us(100);
-		delayAmount -= 100;
-	}
-	while (delayAmount >= 10) {
-		_delay_us(10);
-		delayAmount -= 10;
-	}*/
+		while (delayAmount >= 100) {
+			_delay_us(100);
+			delayAmount -= 100;
+		}
+		while (delayAmount >= 10) {
+			_delay_us(10);
+			delayAmount -= 10;
+		}*/
 
-	// with other overhead, this probably add extra time
-	/*while (delayAmount >= 1) {
-		_delay_us(1);
-		delayAmount -= 1;
-	}*/
+		// with other overhead, this probably add extra time
+		/*while (delayAmount >= 1) {
+			_delay_us(1);
+			delayAmount -= 1;
+		}*/
+	#endif
 }
